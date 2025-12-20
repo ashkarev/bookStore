7008d0e5f7d771b2b7721dda036a4c93f113dedd
Ashkar S <ashkars178@gmail.com>
2025-12-20 21:15:40 +0530

    doing

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -5,6 +5,19 @@ import { FaEdit } from "react-icons/fa";
 const EditProfile = () => {
 const [openModal, setOpenModal] = useState(false);
 
+const [preview,setPreview]=useState("https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80")
+
+const onImageChange=(e)=>{
+console.log(e.target.files[0])  //image stord in here
+
+//img uploading
+
+
+let url= URL.createObjectURL(e.target.files[0])
+console.log(url)
+setPreview(url)
+}
+
   return (
     <>
 
@@ -14,14 +27,19 @@ const [openModal, setOpenModal] = useState(false);
             </button>
 
        <Modal className="mx-60 " show={openModal} onClose={() => setOpenModal(false)}>
-             <ModalHeader className="bg-gray-500 text-3xl text-white p-2">Uploaded Images</ModalHeader>
+             <ModalHeader className="bg-gray-500 text-3xl text-white p-2 ">Uploaded Images</ModalHeader>
              <ModalBody className="bg-gray-600">
                <div className="space-y-6 flex flex-col items-center  ">
+
+                <label htmlFor="imp">
+                    <input onChange={(e)=>onImageChange(e)} type="file" name='' id='imp' />
+                    <img className='w-50' src={preview} alt="" />
+                </label>
                
                 <input className="border p-2 bg-white w-100 " type="text" placeholder="enter name " />
                 <input  className="border p-2 bg-white w-100 " type="text" placeholder="enter passwrod" />
                 <input className="border p-2 bg-white w-100  " type="text" placeholder="confirm password" />
-                <textarea className="border p-2 bg-white w-100 " name="" id="" placeholder="bio"> </textarea>
+                <textarea className="border p-2 bg-white w-100 " name="" id="one" placeholder="bio"> </textarea>
                </div>
              </ModalBody>
              <ModalFooter className="bg-gray-400">
